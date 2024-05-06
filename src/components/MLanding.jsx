8901e28cf4db60233d2bf36c8736a922736561e0
S Sundhar Raj <137048139+sundharAchu00@users.noreply.github.com>
2024-05-06 13:53:49 +0530

    Update MLanding.jsx

--- src/components/MLanding.jsx
+++ src/components/MLanding.jsx
@@ -3,14 +3,7 @@ import SearchIcon from "../search.svg";
 import MovieCard from "./MovieCard";
 //a52df337 api key
 const API_URL = "http://www.omdbapi.com?apikey=a52df337";
-const movie1 = {
-  Title: "Fighting, Flying and Driving: The Stunts of Spiderman 3",
-  Year: "2007",
-  imdbID: "tt1132238",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg",
-};
+
 function MLanding() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
